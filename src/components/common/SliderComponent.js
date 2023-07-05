a8b5dfc0325af6965768e9bf80bd39fd689c4092
pauloFelixx <santospaulor21@gmail.com>
2023-07-05 18:18:20 -0300

    Desenvolvimento do sliderComponent

--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Slider from '@mui/material/Slider';
+import { Stack, Typography } from '@mui/material';
 
-const SliderComponent = () => {
+// Props Destructuring
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
   return (
-    <Slider defaultValue={150} min={100}  max={300} aria-label="Default" valueLabelDisplay="auto" />
-  )
-}
+
+    <Fragment>
+      {/* React.Fragment fake DOM */}
+      <Stack gap={1}>
+        <Typography variant='subtitle2'> Home Value </Typography>
+        <Typography variant='h5'> R$ 3000 </Typography>
+      </Stack>
+      <Slider defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+        value={value}
+      />
+      <Stack direction='row' gap={1} justifyContent={'space-between'}>
+      <Typography color='text.secondary'> R$ 1000 </Typography>
+      <Typography> R$ 10000 </Typography>
+      </Stack>
+    </Fragment>
+  );
+};
 
 export default SliderComponent
