3756879cce74521ef74ccc0eca7bd534b78d6ec9
pauloFelixx <santospaulor21@gmail.com>
2023-07-14 23:06:32 -0300

    results

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,30 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react'
 
-const Result = () => {
-  return (
-    <div>Result</div>
-  )
-}
+const Result = ({ homeValue, downPayment, loanAmount, loanTerm, interestRate }) => {
+  //const { homeValue, downPayment, loanAmount, loanTerm, interestRate } = data;
 
-export default Result
+  const totalLoanMonths = loanTerm * 12; // 60 months = 5 years
+  const interestPerMonth = interestRate / 100 / 12; // 100 @ 12% for 1 year
+
+  const monthlyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  console.log(totalInterestGenerated);
+
+
+  return (
+    <Stack>
+      <Typography>
+        Monthly Payment: $ {monthlyPayment}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default Result;
